d649d10e92c338bf5c7859ca0443591333c76671
CharleneNgiam <s10227262@connect.np.edu.sg>
2022-02-09 02:03:13 +0800

    testing to get data from json

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ document.addEventListener("DOMContentLoaded", (e)=>{
         splash.classList.add("display-none");
     }, 2000);
 })
+
+/* for the top news article to appear from api*/
+let url = "https://newsapi.org/v2/top-headlines?country=sg&apiKey=784219b208334c7a9c94e2d8ab4a2d20";
+
+fetch(url)
+  .then(response => response.json())
+  .then(function(data){
+    $("#article").html("");
+    $("#article").html(data.articles[0].title.url);
+  })

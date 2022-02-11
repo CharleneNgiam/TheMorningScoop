0dc94fff2bcdf06adecde7265191ce8ae4fed885
CharleneNgiam <s10227262@connect.np.edu.sg>
2022-02-11 21:37:15 +0800

    trying out API with js

--- index.js
+++ index.js
@@ -7,12 +7,40 @@ document.addEventListener("DOMContentLoaded", (e)=>{
     }, 2000);
 })
 
-/* for the top news article to appear from api*/
-let url = "https://newsapi.org/v2/top-headlines?country=sg&apiKey=784219b208334c7a9c94e2d8ab4a2d20";
 
-fetch(url)
-  .then(response => response.json())
-  .then(function(data){
-    $("#article").html("");
-    $("#article").html(data.articles[0].title.url);
+/* for the top news article to appear from api*/
+$(document).ready(function(){
+
+  let url = "https://newsapi.org/v2/top-headlines?country=sg&apiKey=784219b208334c7a9c94e2d8ab4a2d20";
+
+  $.ajax({
+    url:url,
+    method:"GET",
+    dataType:"Json",
+
+    success: function(news){
+      let output = "";
+      let latestNews = news.articles;
+
+      for(var i in latestNews){
+        output +=`
+          <div class="col 13 m6 s12">
+            <h3>${latestNews[i].title}</h3>
+          </div>
+        `;
+      }
+
+      if(output !== ""){
+        $("#newsResults").html(output);
+      }
+
+    },
+    error: function(){
+
+    }
   })
+
+
+});
+
+
